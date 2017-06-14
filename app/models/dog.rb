f91a221ca14e8c89cb9a358097d9e1fec204c665
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-14 08:06:28 -0500

    Update show view for owner so lists all dogs by owner, order dog index by name, add validation to both dogs and owners

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :owner
+
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :age, presence: true, numericality: true
 end
